6b2351b61e1e35cb854dee6b053fec51bb5fe3c9
Daniel Saavedra <mantrax3.14@gmail.com>
2025-07-08 19:25:42 -0600

    google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="IR_DmTAqGl5tE0iYlicq2SIJiqz408fqQ2uoRHeZcdE" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
